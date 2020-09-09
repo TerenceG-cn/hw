349fcf9d139f27d32759589deaec46abe0f1c519
Terence-cn <1035260902@qq.com>
2020-09-09 14:27:10 +0800

    commit

--- src/main/java/com/tce/Main.java
+++ src/main/java/com/tce/Main.java
@@ -1,8 +1,35 @@
 package com.tce;
 
+import sun.reflect.generics.tree.Tree;
+
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("nmsl");
+
+        //System.out.println("nmsl");
+//        Thread thread=new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        });
+//        thread.start();
+
+        ArrayList<String> arrayList=new ArrayList<>();
+        arrayList.add("das");
+        arrayList.add("dase34");
+        arrayList.add("daswe");
+        arrayList.add("das");
+
+        //arrayList.sort();
+       HashMap<String,Integer> hp=new HashMap<>();
+
+        int hash=402348261;
+        int len=16;
+        System.out.println((hash)%(len));
+        System.out.println((len-1)&hash);
     }
+
 }
