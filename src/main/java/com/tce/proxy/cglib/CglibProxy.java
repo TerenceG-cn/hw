d2983aecb25dc7f32a1128996e245a7e544f1acc
Terence-cn <1035260902@qq.com>
2020-09-15 08:15:01 +0800

    cglib

--- src/main/java/com/tce/proxy/cglib/CglibProxy.java
+++ src/main/java/com/tce/proxy/cglib/CglibProxy.java
@@ -12,6 +12,7 @@ public class CglibProxy implements MethodInterceptor
     // 根据一个类型产生代理类，此方法不要求一定放在MethodInterceptor中
     public Object CreatProxyedObj(Class<?> clazz)
     {
+        //增强
         Enhancer enhancer = new Enhancer();
 
         enhancer.setSuperclass(clazz);
@@ -22,11 +23,36 @@ public class CglibProxy implements MethodInterceptor
     }
 
     @Override
+    //拦截
     public Object intercept(Object arg0, Method arg1, Object[] arg2, MethodProxy arg3) throws Throwable
     {
         // 这里增强
         System.out.println("收钱");
 
         return arg3.invokeSuper(arg0, arg2);
+    }
+
+    public static void main(String[] args)
+    {
+        int times = 1000000;
+
+        CglibProxy proxy2 = new CglibProxy();
+        long time5 = System.currentTimeMillis();
+        Star star2 = (Star)proxy2.CreatProxyedObj(LiuDeHua.class);
+        long time6 = System.currentTimeMillis();
+        System.out.println("cglib创建时间：" + (time6 - time5));
+
+
+        long time7 = System.currentTimeMillis();
+        for (int i = 1; i <= times; i++)
+        {
+            star2.sing("ss");
+
+            star2.dance("ss");
+        }
+
+        long time8 = System.currentTimeMillis();
+
+        System.out.println("cglib执行时间" + (time8 - time7));
     }
 }
---
+++ src/main/java/com/tce/proxy/cglib/Star.java
@@ -0,0 +1,8 @@
+package com.tce.proxy.cglib;
+
+public interface Star
+{
+    String sing(String name);
+
+    String dance(String name);
+}
